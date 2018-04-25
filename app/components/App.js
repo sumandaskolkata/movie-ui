c9f8dade4a5e38cad01327b2f0894e5bbfe78f90
INsumand <insumand@INsumands-MacBook-Pro.local>
2018-04-25 16:30:42 +0530

    Suman:Navya | Initial commit.

---
+++ app/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './App.scss';
+
+
+export default function App() {
+    return <div className="App">Hello World !!!</div>
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack =require('webpack');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    devtool: "inline-source-map",
+    entry: ['./app/index.js'],
+    output: {
+        path: path.join(__dirname,'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+        {
+            test: /.js$/,
+            loader: 'babel-loader',
+            include: path.join(__dirname,'app'),
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015','react']
+            }
+        },
+        {
+            test: /\.scss$/,
+            include: path.join(__dirname,'app'),
+            loaders: ["style-loader", "css-loader", "sass-loader"]
+        },
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './app/public/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
